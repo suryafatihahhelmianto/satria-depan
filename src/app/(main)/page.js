896bef59e0990c750d851228572cb6cfeda96497
Surya Fatihah Helmianto <95084814+suryafatihahhelmianto@users.noreply.github.com>
2024-11-20 14:36:34 +0700

    RESPONSIF
    supaya mas wasis senang

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,27 +1,25 @@
 "use client";
 
-import HistogramChart from "@/components/HistogramChart";
-import InfoButton from "@/components/InfoButton";
-import SustainabilityIndexChart from "@/components/SustainabilityIndexChart";
-import { fetchData } from "@/tools/api";
-import { getCookie } from "@/tools/getCookie";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
-import "react-circular-progressbar/dist/styles.css"; // Import CSS untuk Circular Progress Bar
-import DatePicker from "react-datepicker"; // Import the DatePicker component
-import "react-datepicker/dist/react-datepicker.css"; // Import DatePicker CSS
-import { AiOutlineInfoCircle } from "react-icons/ai";
+import DatePicker from "react-datepicker";
 import { AiOutlineExclamationCircle } from "react-icons/ai";
 import { BsFillCircleFill } from "react-icons/bs";
+import HistogramChart from "@/components/HistogramChart";
+import InfoButton from "@/components/InfoButton";
+import { fetchData } from "@/tools/api";
+import { getCookie } from "@/tools/getCookie";
+
+import "react-circular-progressbar/dist/styles.css";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function HomePage() {
   const [selectedYear, setSelectedYear] = useState(2021);
   const [factories, setFactories] = useState([]);
   const [selectedFactory, setSelectedFactory] = useState({});
   const [sustainabilityData, setSustainabilityData] = useState([]);
-  const [selectedDate, setSelectedDate] = useState(new Date()); // State for the selected date
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const router = useRouter();
 
@@ -64,12 +62,12 @@ export default function HomePage() {
       const response = await fetchData("/api/pabrik", {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${cookie}`, // Ambil token dari localStorage
+          Authorization: `Bearer ${cookie}`,
         },
       });
 
       if (response.length > 0) {
-        setSelectedFactory(response[0]); // Set sebagai objek pabrik
+        setSelectedFactory(response[0]);
       }
       setFactories(response);
     } catch (error) {
@@ -98,7 +96,6 @@ export default function HomePage() {
   const handleDetailClick = async () => {
     const sesiPengisianId = await fetchSesiPengisian();
     if (sesiPengisianId) {
-      // Navigate to the detail page with the sesiPengisianId
       router.push(`/detail/${sesiPengisianId}/sumber-daya`);
     } else {
       alert("Sesi pengisian tidak ditemukan");
@@ -110,15 +107,15 @@ export default function HomePage() {
   }, []);
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
-      <div className="flex justify-between">
-        <h1 className="text-3xl font-semibold mb-6 text-green-700">
+    <div className="p-4 md:p-6 bg-gray-100 min-h-screen">
+      <div className="flex flex-col md:flex-row md:justify-between mb-6">
+        <h1 className="text-2xl md:text-3xl font-semibold mb-4 md:mb-0 text-green-700">
           Kondisi Pabrik{" "}
           {selectedFactory ? selectedFactory.namaPabrik : "Pabrik"} saat ini
         </h1>
-        <div className="mb-4 flex justify-end items-center gap-2">
+        <div className="mb-4 flex flex-col md:flex-row md:items-center gap-2">
           <label className="block text-lg font-semibold">Pilih Pabrik:</label>
-          <div className="flex space-x-4">
+          <div className="flex flex-wrap gap-2">
             {factories.map((factory) => (
               <label
                 key={factory.id}
@@ -133,7 +130,7 @@ export default function HomePage() {
                   value={factory.id}
                   checked={selectedFactory?.id === factory.id}
                   onChange={() => setSelectedFactory(factory)}
-                  className="hidden" // Hide the default radio button
+                  className="hidden"
                 />
                 <span className="text-center">{factory.namaPabrik}</span>
               </label>
@@ -142,29 +139,19 @@ export default function HomePage() {
         </div>
       </div>
 
-      {/* Main Grid */}
-      <div className="grid grid-cols-2 gap-4">
-        {/* Gabungan Circular Progress Kinerja dan Status Data */}
-        <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white p-4 md:p-6 rounded-lg shadow-md flex flex-col items-center">
           <div className="w-full mb-4">
             <h1 className="text-xl font-semibold">
               Nilai Kinerja Keberlanjutan Rantai Pasok
             </h1>
           </div>
 
-          {/* Bagian Kiri: Detail & Circular Progress */}
-          <div className="flex justify-around gap-5 w-full h-full">
-            {/* Bagian Kiri: Circular Progress */}
+          <div className="flex flex-col md:flex-row justify-around gap-5 w-full h-full">
             <div className="flex flex-col items-center justify-end mb-6 h-full">
-              {/* <button
-      onClick={handleDetailClick}
-      className="mb-4 font-semibold text-xl bg-gray-300 px-4 py-2 rounded-lg"
-    >
-      Detail
-    </button> */}
               <div
                 onClick={handleDetailClick}
-                className="w-52 h-52 mb-2 hover:cursor-pointer"
+                className="w-40 h-40 md:w-52 md:h-52 mb-2 hover:cursor-pointer"
               >
                 <CircularProgressbar
                   value={progressKinerja}
@@ -180,14 +167,12 @@ export default function HomePage() {
                 <p className="mt-2 text-gray-700 text-center font-semibold">
                   BERKELANJUTAN
                 </p>
-                <InfoButton /> {/* Replace the icon with InfoButton */}
+                <InfoButton />
               </div>
             </div>
 
-            {/* Garis Pemisah */}
-            <div className="border-l-2 border-gray-300 h-full"></div>
+            <div className="border-t-2 md:border-l-2 md:border-t-0 border-gray-300 w-full md:w-0 h-0 md:h-full my-4 md:my-0"></div>
 
-            {/* Bagian Kanan: Status Data */}
             <div>
               <div className="font-semibold mb-4">
                 <h1>Periode Perhitungan:</h1>
@@ -208,7 +193,6 @@ export default function HomePage() {
                 Status Perhitungan:
               </p>
               <div className="flex justify-center space-x-4">
-                {/* Lingkaran dengan tulisan FINAL */}
                 <div className="flex items-center justify-center w-24 h-24 rounded-full bg-green-500 text-white font-bold">
                   <p>FINAL</p>
                 </div>
@@ -217,25 +201,21 @@ export default function HomePage() {
           </div>
         </div>
 
-        <div className="bg-white p-6 rounded-lg shadow-md flex flex-col items-center">
+        <div className="bg-white p-4 md:p-6 rounded-lg shadow-md flex flex-col items-center">
           <div className="w-full mb-4">
             <h1 className="text-xl font-semibold">Nilai Prediksi Rendemen</h1>
           </div>
 
-          <div className="flex justify-around gap-5 w-full h-full">
-            {/* Bagian Kiri: Circular Progress dengan warna dinamis */}
+          <div className="flex flex-col md:flex-row justify-around gap-5 w-full h-full">
             <div className="flex flex-col items-center justify-center mb-6 h-full">
               <div className="flex flex-col items-center justify-center mb-6 h-full">
-                {/* Nilai Rendemen */}
                 <div className="text-5xl font-bold mb-4">7.5%</div>
 
-                {/* Bar dengan Gradient */}
-                <div className="relative w-60 h-9 rounded-full bg-gradient-to-r from-red-500 via-yellow-500 to-green-500">
-                  {/* Indikator Nilai */}
+                <div className="relative w-full md:w-60 h-9 rounded-full bg-gradient-to-r from-red-500 via-yellow-500 to-green-500">
                   <div
                     className="absolute top-1/2 transform -translate-y-1/2 -translate-x-1/2 border-2 border-gray-700 bg-transparent"
                     style={{
-                      left: `${(7.5 / 12) * 100}%`, // Posisi indikator berdasarkan nilai
+                      left: `${(7.5 / 12) * 100}%`,
                       width: "30px",
                       height: "50px",
                     }}
@@ -244,10 +224,8 @@ export default function HomePage() {
               </div>
             </div>
 
-            {/* Garis Pemisah */}
-            <div className="border-l-2 border-gray-300 h-full"></div>
+            <div className="border-t-2 md:border-l-2 md:border-t-0 border-gray-300 w-full md:w-0 h-0 md:h-full my-4 md:my-0"></div>
 
-            {/* Bagian Kanan: Status Data */}
             <div className="flex flex-col items-center mb-6 h-full">
               <div className="font-semibold mb-4 text-black">
                 <h1>Periode Perhitungan:</h1>
@@ -256,92 +234,17 @@ export default function HomePage() {
                 <DatePicker
                   selected={selectedDate}
                   onChange={(date) => setSelectedDate(date)}
-                  inline // Kalender selalu terlihat
+                  inline
                   dateFormat="dd/MM/yyyy"
                   className="border border-white/20 rounded-md p-2 bg-white/40 text-green-700 text-lg"
                 />
               </div>
             </div>
           </div>
-          {/* <div className="flex justify-around items-center w-full bg-blue-300">
-            <div className="flex flex-col gap-2 bg-green-200 h-full justify-end">
-              <div className="w-52 h-52 mb-2">
-                <CircularProgressbar
-                  value={6}
-                  text={`${6}%`}
-                  styles={buildStyles({
-                    pathColor: "#4CAF50",
-                    textColor: "#4CAF50",
-                    trailColor: "#d6d6d6",
-                  })}
-                />
-              </div>
-              <h2 className="text-xl font-semibold mb-2 text-center">
-                Rendemen
-              </h2>
-            </div>
-
-            <div className="border-l-2 border-gray-300 h-full"></div>
-
-            <div className="flex flex-col items-center justify-center">
-              <h1 className="text-left font-semibold w-full mb-2">
-                Periode Perhitungan:
-              </h1>
-              <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                inline // Makes the calendar always visible
-                dateFormat="dd/MM/yyyy"
-                className="border border-gray-300 rounded-md p-2"
-              />
-            </div>
-          </div> */}
         </div>
-
-        {/* Inline Calendar */}
-        {/* <div className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center">
-          <div className="w-full mb-4">
-            <h1 className="font-semibold">Nilai Prediksi Rendemen</h1>
-          </div>
-          <div className="flex justify-around items-center w-full bg-blue-300">
-            <div className="flex flex-col gap-2 bg-green-200 h-full justify-end">
-              <div className="w-44 h-44 mb-2">
-                <CircularProgressbar
-                  value={6}
-                  text={`${6}%`}
-                  styles={buildStyles({
-                    pathColor: "#4CAF50",
-                    textColor: "#4CAF50",
-                    trailColor: "#d6d6d6",
-                  })}
-                />
-              </div>
-              <h2 className="text-xl font-semibold mb-2 text-center">
-                Rendemen
-              </h2>
-            </div>
-
-            <div className="border-l-2 border-gray-300 h-full"></div>
-
-            <div className="flex flex-col items-center justify-center">
-              <h1 className="text-left font-semibold w-full mb-2">
-                Periode Perhitungan:
-              </h1>
-              <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                inline // Makes the calendar always visible
-                dateFormat="dd/MM/yyyy"
-                className="border border-gray-300 rounded-md p-2"
-              />
-            </div>
-          </div>
-        </div> */}
       </div>
 
-      {/* Lower Grid */}
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 mt-6">
-        {/* Line Chart for Kinerja */}
         <div className="bg-white p-4 rounded-lg shadow-md">
           <h2 className="text-xl font-semibold mb-2 text-center">
             Kinerja Keberlanjutan Rantai Pasok{" "}
@@ -351,12 +254,9 @@ export default function HomePage() {
           <HistogramChart data={dataHistogram} />
         </div>
 
-        {/* Informasi */}
         <div className="relative bg-white border-l-8 border-gray-300 p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
-          {/* Enhanced Notebook lines */}
           <div className="absolute inset-0 bg-[repeating-linear-gradient(black, black 34px, #e0e0e0 36px)] rounded-xl pointer-events-none"></div>
 
-          {/* Content */}
           <div className="relative">
             <h2 className="text-2xl font-bold text-gray-800 mb-4 flex items-center">
               <AiOutlineExclamationCircle className="mr-3 text-gray-500" />
