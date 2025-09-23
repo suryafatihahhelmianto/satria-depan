71def117e59f79b5053ed461630dc8da9b8bb57c
root <root@satria-ipb-rajawali2.co.id>
2025-09-23 16:40:34 +0700

    Change URL to Production

--- src/tools/api.js
+++ src/tools/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_DEV,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL_PROD,
   // timeout: 10000, // Timeout if necessary
   headers: {
     ContentType: "application/json",
